5df1bad45b124f60274eb3a91c0573f313535e53
BryanBrantl <bryanbrantl13@gmail.com>
2025-05-24 15:59:44 -0300

    Delete pages directory

--- pages/1_Pagina1.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Página 1")
-st.write("Esta é a primeira página.")
--- pages/2_Pagina2.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Página 2")
-st.write("Esta é a segunda página.")
