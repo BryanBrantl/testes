34970f5f511f905438b6d66975fa0b1db6d0433f
BryanBrantl <bryanbrantl13@gmail.com>
2025-05-28 12:08:38 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -71,16 +71,16 @@ if selected == "Home":
             MEMBROS
         </h1>
     """, unsafe_allow_html=True)
-
-with col1:
-    st.image(img1, caption='Coluna 1')
-
-with col2:
-    st.image(img2, caption='Coluna 2 (mesma imagem, parecerá menor se a imagem original for grande)')
-
-# Você também pode usar o parâmetro 'width' dentro das colunas
-with col3:
-    st.image(img3, caption='Coluna 3 (width=100)', width=100)
+    
+    with col1:
+        st.image(img1, caption='Coluna 1')
+    
+    with col2:
+        st.image(img2, caption='Coluna 2 (mesma imagem, parecerá menor se a imagem original for grande)')
+    
+    # Você também pode usar o parâmetro 'width' dentro das colunas
+    with col3:
+        st.image(img3, caption='Coluna 3 (width=100)', width=100)
 
 ############################################################################################
 ######################################   BIOMOVE    ########################################
