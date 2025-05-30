504723c7b4054cca3b3267ab94237f7e00f3c78f
BryanBrantl <bryanbrantl13@gmail.com>
2025-05-30 14:22:57 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as stMore actions
 from streamlit_option_menu import option_menu
 from PIL import Image
 
@@ -89,7 +89,7 @@ if selected == "Home":
             HOME
         </h1>
     """, unsafe_allow_html=True)
-    
+
     st.markdown("""
         <h1 style='text-align: center; color: #008080 ;'>
             MEMBROS
@@ -123,7 +123,7 @@ elif selected == "BioMove":
 
 elif selected == "Atualização Semanal":
     st.title("Atualizações Semanais")
-    
+
     def bloco_atualizacao(titulo, texto):
         st.markdown(f"""
             <div style="background-color:#1e1e1e; padding:20px; margin-bottom:10px; border-radius:10px; border-left: 5px solid teal;">
@@ -131,7 +131,7 @@ elif selected == "Atualização Semanal":
                 <p style="color:gray;">{texto}</p>
             </div>
         """, unsafe_allow_html=True)
-     
+
     bloco_atualizacao(" Semana 2 - 27/05/2025", """
     - Definido a utilização de um Kit chassi para a construção do carrinho. <br>
     - Montagem do sensor EMG em protoboard baseado no esquemático da semana anterior. <br>
@@ -139,7 +139,7 @@ elif selected == "Atualização Semanal":
     - Definido a necessidade de implementar mais filtros no circuito para reduzir o ruído. <br>
     - Circuito montado em protoboard: <br>
     """)
-    
+
     bloco_atualizacao(" Semana 1 - 20/05/2025", """
    - Realizados testes nos principais componentes do carrinho (motor DC, ESP32 e ponte H), sem identificação de defeitos.<br>
    - Conduzido estudo sobre softwares de modelagem 3D. Optou-se pela utilização do Eagle para o desenvolvimento do carrinho e do sistema EMG.<br>
@@ -148,7 +148,7 @@ elif selected == "Atualização Semanal":
    - Decisões sobre o projeto: Definido que será utilizado baterias 18650 (4.2v) para alimentação dos sistemas.<br>
    - Construção do esquemático EMG:
     """)
-    
+
 
 ############################################################################################
 ###################################   RELATORIOS    ########################################
@@ -196,25 +196,24 @@ Acesse o documento completo para mais detalhes.""", # \n será convertido para <
 ############################################################################################
 ###################################   CRONOGRAMA    ########################################
 elif selected == "Cronograma":
-     st.markdown("""
-            <h1 style='text-align: center; color: #008080 ;'>
-                CRONOGRAMA - alterar
-            </h1>
-        """, unsafe_allow_html=True)
-    
-    
-    # URL fornecida para o relatório final
-   link = "https://docs.google.com/spreadsheets/d/1Fb5_otX8z50tuy9RbcGGC89BLKfErs_SCnML-JGeyQU/edit?usp=sharing"
+ st.markdown("""
+        <h1 style='text-align: center; color: #008080 ;'>
+            CRONOGRAMA - alterar
+        </h1>
+    """, unsafe_allow_html=True)
 
-    # Chamando a função para criar o bloco do Relatório Final
-    bloco_relatorio_final(
-        titulo_bloco="Relatório Final do Projeto",
-        texto_descricao="""O relatório final consolida todos os resultados, análises e conclusões do projeto BioMove.
-Acesse o documento completo para mais detalhes.""", # \n será convertido para <br>
-        texto_botao="ABRIR RELATÓRIO FINAL",
-        url_botao=link
-    )
 
+ link3 = "https://docs.google.com/spreadsheets/d/1Fb5_otX8z50tuy9RbcGGC89BLKfErs_SCnML-JGeyQU/edit?usp=sharing"
+
+ bloco_relatorio_final(
+     titulo_bloco="Relatório Parcial Q1",
+     texto_descricao="Este relatório cobre as atividades e progresso do primeiro trimestre do projeto.",
+     texto_botao="ABRIR RELATÓRIO PARCIAL Q1",
+     url_botao=link_relatorio_parcial
+     url_botao=link3
+ )
+
+# Rodapé
 st.markdown("""
     <style>
         .footer {
