4fe0d5519a8ad54e53a60827605f3691c8b8261c
BryanBrantl <bryanbrantl13@gmail.com>
2025-06-11 12:01:18 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -75,61 +75,61 @@ if selected == "Home":
     """, unsafe_allow_html=True)
 
     col1, col2, col3, col4 = st.columns(4)  
-with col1:
-    st.image(img1, width=300)
-    st.markdown(
-        """
-        <div style='text-align: center'>
-            <strong>Bryan Alexandre de Lima Brantl</strong><br>
-            RA: 2414139<br>
-            E-mail: brantl@alunos.utfpr.edu.br<br>
-            Contato: (41) 99742-4536
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
-with col2:
-    st.image(img2, width=300)
-    st.markdown(
-        """
-        <div style='text-align: center'>
-            <strong>João Roberto Klassen</strong><br>
-            RA: 2414155<br>
-            E-mail: joaoklassen@alunos.utfpr.edu.br<br>
-            Contato: (41) 99742-4536
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
-with col3:
-    st.image(img3, width=300)
-    st.markdown(
-        """
-        <div style='text-align: center'>
-            <strong>Leonardo Amancio</strong><br>
-            RA: 2402580<br>
-            E-mail: leonardoamancio@alunos.utfpr.edu.br<br>
-            Contato: (41) 99805-1279
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
-with col4:
-    st.image(img4, width=300)
-    st.markdown(
-        """
-        <div style='text-align: center'>
-            <strong>Luiz Prado</strong><br>
-            RA: 2402629<br>
-            E-mail: luizoliveira.2002@alunos.utfpr.edu.br<br>
-            Contato: (41) 99815-6532
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+    with col1:
+        st.image(img1, width=300)
+        st.markdown(
+            """
+            <div style='text-align: center'>
+                <strong>Bryan Alexandre de Lima Brantl</strong><br>
+                RA: 2414139<br>
+                E-mail: brantl@alunos.utfpr.edu.br<br>
+                Contato: (41) 99742-4536
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+    
+    with col2:
+        st.image(img2, width=300)
+        st.markdown(
+            """
+            <div style='text-align: center'>
+                <strong>João Roberto Klassen</strong><br>
+                RA: 2414155<br>
+                E-mail: joaoklassen@alunos.utfpr.edu.br<br>
+                Contato: (41) 99742-4536
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+    
+    with col3:
+        st.image(img3, width=300)
+        st.markdown(
+            """
+            <div style='text-align: center'>
+                <strong>Leonardo Amancio</strong><br>
+                RA: 2402580<br>
+                E-mail: leonardoamancio@alunos.utfpr.edu.br<br>
+                Contato: (41) 99805-1279
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+    
+    with col4:
+        st.image(img4, width=300)
+        st.markdown(
+            """
+            <div style='text-align: center'>
+                <strong>Luiz Prado</strong><br>
+                RA: 2402629<br>
+                E-mail: luizoliveira.2002@alunos.utfpr.edu.br<br>
+                Contato: (41) 99815-6532
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 ############################################################################################
 ######################################   BIOMOVE    ########################################
 
