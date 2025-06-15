9ba03cbbb50d9880a1ddf7bd37eb26c8e3ce84ac
BryanBrantl <bryanbrantl13@gmail.com>
2025-06-14 22:03:56 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -240,7 +240,7 @@ elif selected == "Atualização Semanal":
 			<ul>
 	            </div>
 	        """, unsafe_allow_html=True)
-		A0_col1_1, A0_col1_2 = st.columns([1,5])
+		A0_col1_1, A0_col1_2 = st.columns([1,9])
 		with A0_col1_1:
 			st.write(f"CIRCUITO EMG:")
 		with A0_col1_2:
